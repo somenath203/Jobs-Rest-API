8fc2f7b0fd433339a6fa63286e40662588305793
Somenath Choudhury <somenathchoudhury35@gmail.com>
2022-07-10 16:40:18 +0530

    added swagger docs

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
 
+// swagger configuration
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+
+
 const app = express();
 
 const authRouter = require('./routes/auth');
@@ -21,6 +29,8 @@ const authenticateUser = require('./middleware/authentication');
 
 app.use(express.json());
 
+app.use(express.static('./public'));
+
 app.use(helmet());
 app.use(cors());
 app.use(xss());
@@ -31,12 +41,12 @@ app.use(rateLimit({
   max: 100
 }));
 
-
 app.get('/', (req, res) => {
-  res.status(200).json({
-    welcomeMsg: "Welcome to Jobs RestAPI"
-  });
+  res.status(200).sendFile('index.html');
 });
+
+// swagger docs middleware
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
